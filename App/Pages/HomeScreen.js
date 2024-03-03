330460501451755f92d3bfd6f0b538b73cef082a
The-Abhishek1 <idiot63666@gmail.com>
2024-03-03 09:38:36 +0530

    Fixed Loading Issue

--- App/Pages/HomeScreen.js
+++ App/Pages/HomeScreen.js
@@ -5,6 +5,7 @@ import {
   FlatList,
   TouchableOpacity,
   Dimensions,
+  ActivityIndicator,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import ChatFaceData from "../Services/ChatFaceData";
@@ -12,7 +13,7 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
   const [chatFaceData, setChatFacedata] = useState();
-  const [selectChatFaceData, setSelectChatFaceData] = useState();
+  const [selectChatFaceData, setSelectChatFaceData] = useState([]);
 
   const navigation = useNavigation();
   useEffect(() => {
@@ -26,72 +27,90 @@ export default function HomeScreen() {
 
   return (
     <View style={{ alignItems: "center", paddingTop: 90 }}>
-      <Text style={[{ color: selectChatFaceData.primary }, { fontSize: 30 }]}>
-        Hello
-      </Text>
-      <Text
-        style={[
-          { color: selectChatFaceData.primary },
-          { fontSize: 30, fontWeight: "bold" },
-        ]}
-      >
-        I am {selectChatFaceData.name}
-      </Text>
-      <Image
-        source={{ uri: selectChatFaceData.image }}
-        style={{ width: 150, height: 150, marginTop: 20, borderRadius: 100 }}
-      />
-      <Text style={{ marginTop: 30, fontSize: 25 }}>How can I help you?</Text>
-      <View
-        style={{
-          marginTop: 20,
-          backgroundColor: "#F5F5F5",
-          alignItems: "center",
-          padding: 10,
-          height: 140,
-          borderRadius: 30,
-        }}
-      >
-        <FlatList
-          data={chatFaceData}
-          horizontal={true}
-          renderItem={({ item }) =>
-            selectChatFaceData.id != item.id && (
-              <TouchableOpacity
-                style={{ margin: 13 }}
-                onPress={() => {
-                  onChatFacePress(item.id);
-                }}
-              >
-                <Image
-                  source={{ uri: item.image }}
-                  style={{ width: 70, height: 70, borderRadius: 40 }}
-                />
-              </TouchableOpacity>
-            )
-          }
+      {selectChatFaceData.length == 0 ? (
+        <ActivityIndicator
+          size={"large"}
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         />
-        <Text style={{ marginTop: 5, fontSize: 17, color: "#808080" }}>
-          Choose Your Fav ChatBuddy
-        </Text>
-      </View>
-      <TouchableOpacity
-        style={[
-          { backgroundColor: selectChatFaceData.primary },
-          {
-            padding: 17,
-            width: Dimensions.get("screen").width * 0.6,
-            borderRadius: 100,
-            alignItems: "center",
-            marginTop: 40,
-          },
-        ]}
-        onPress={() =>
-          navigation.navigate("chat", { selectedFace: selectChatFaceData })
-        }
-      >
-        <Text style={{ fontSize: 16, color: "#fff" }}>Let's Chat</Text>
-      </TouchableOpacity>
+      ) : (
+        <>
+          <Text
+            style={[{ color: selectChatFaceData.primary }, { fontSize: 30 }]}
+          >
+            Hello
+          </Text>
+          <Text
+            style={[
+              { color: selectChatFaceData.primary },
+              { fontSize: 30, fontWeight: "bold" },
+            ]}
+          >
+            I am {selectChatFaceData.name}
+          </Text>
+          <Image
+            source={{ uri: selectChatFaceData.image }}
+            style={{
+              width: 150,
+              height: 150,
+              marginTop: 20,
+              borderRadius: 100,
+            }}
+          />
+          <Text style={{ marginTop: 30, fontSize: 25 }}>
+            How can I help you?
+          </Text>
+          <View
+            style={{
+              marginTop: 20,
+              backgroundColor: "#F5F5F5",
+              alignItems: "center",
+              padding: 10,
+              height: 140,
+              borderRadius: 30,
+            }}
+          >
+            <FlatList
+              data={chatFaceData}
+              horizontal={true}
+              renderItem={({ item }) =>
+                selectChatFaceData.id != item.id && (
+                  <TouchableOpacity
+                    style={{ margin: 13 }}
+                    onPress={() => {
+                      onChatFacePress(item.id);
+                    }}
+                  >
+                    <Image
+                      source={{ uri: item.image }}
+                      style={{ width: 70, height: 70, borderRadius: 40 }}
+                    />
+                  </TouchableOpacity>
+                )
+              }
+            />
+            <Text style={{ marginTop: 5, fontSize: 17, color: "#808080" }}>
+              Choose Your Fav ChatBuddy
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={[
+              { backgroundColor: selectChatFaceData.primary },
+              {
+                padding: 17,
+                width: Dimensions.get("screen").width * 0.6,
+                borderRadius: 100,
+                alignItems: "center",
+                marginTop: 40,
+              },
+            ]}
+            onPress={() =>
+              navigation.navigate("chat", { selectedFace: selectChatFaceData })
+            }
+          >
+            <Text style={{ fontSize: 16, color: "#fff" }}>Let's Chat</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 }
